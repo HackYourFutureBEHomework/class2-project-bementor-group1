1bb320c05a689360c5a15587cde1eedbb80ba510
HackYourFuture Belgium <manjuparbha@gmail.com>
2019-04-02 23:45:45 +0200

    some update for nav bar after merge and resolving conflicts

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -5,15 +5,31 @@ class Header extends Component {
     return (
       <div>
         <header className="header">
-          <h1 className="header-title">BeMentor.</h1>
+          <h1 className="title">BeMentor.</h1>
         </header>
 
         <nav>
           <ul className="header-navbar">
-            <li className="header-nav-button">CONNECT</li>
-            <li className="header-nav-button">CONTACT</li>
-            <li className="header-nav-button">SEARCH MENTORS</li>
-            <li className="header-nav-button">LOGIN</li>
+            <li className="header-nav-button">
+              <a href="/Home" title="Home">
+                Home
+              </a>
+            </li>
+            <li className="header-nav-button">
+              <a href="/contact" title="Contact">
+                Contact
+              </a>
+            </li>
+            <li className="header-nav-button">
+              <a href="/Mentors" title="Search Mentor">
+                Search
+              </a>
+            </li>
+            <li className="header-nav-button">
+              <a href="/Login" title="Login">
+                Login
+              </a>
+            </li>
           </ul>
         </nav>
       </div>
