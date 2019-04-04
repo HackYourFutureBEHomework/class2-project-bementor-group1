910f2a5b3cd0408e64119b0e06647a0dccbfdc0f
Joel <camacho-joel@hotmail.com>
2019-04-04 21:32:33 +0200

    create user form for editing profile

---
+++ client/src/components/UserForm.jsx
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import User from "../../../models/User";
+
+class ProfileEditor extends Component {
+  state = {};
+
+  submitEdits = e => {
+    e.preventDefault();
+  };
+
+  render() {
+    const { user } = this.props;
+    return (
+      <form onSubmit={this.submitEdits}>
+        <input type="submit" />
+      </form>
+    );
+  }
+}
+
+ProfileEditor.propTypes = {
+  user: PropTypes.instanceOf(User).isRequired
+};
+
+export default ProfileEditor;
