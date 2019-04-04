596880c48ce85d42d0fca54bce17e51a95ddf224
Joel <camacho-joel@hotmail.com>
2019-04-04 22:26:38 +0200

    give structure to form

--- client/src/components/UserForm.jsx
+++ client/src/components/UserForm.jsx
@@ -1,18 +1,52 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import User from "../models/User";
+import { TextInputField } from "evergreen-ui";
 
 class ProfileEditor extends Component {
-  state = {};
+  state = {
+    fields: {}
+  };
 
   submitEdits = e => {
     e.preventDefault();
   };
 
+  handleFieldChanged = e => {
+    this.setState({
+      fields: {
+        [e.currentTarget.name]: {
+          value: e.currentTarget.value,
+          validationMessage: null
+        }
+      }
+    });
+  };
+
+  getValidationMessage = field => {
+    const { fields } = this.state;
+    if (!fields[field]) return "";
+    return fields[field].validationMessage;
+  };
+
+  isInvalid = field => !!this.getValidationMessage(field);
+
   render() {
     const { user } = this.props;
+    const { fields } = this.state;
+
     return (
       <form onSubmit={this.submitEdits}>
+        <TextInputField
+          label="First name"
+          name="firstName"
+          defaultValue={user.firstName}
+          value={fields.firstName}
+          isInvalid={!!this.isInvalid("firstName")}
+          validationMessage={!!this.getValidationMessage("firstName")}
+          onChange={this.handleFieldChanged}
+          required
+        />
         <input type="submit" />
       </form>
     );
