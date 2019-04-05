27b501e2d7ebbc713e87c702b062b3fb520e2fdb
HackYourFuture Belgium <manjuparbha@gmail.com>
2019-04-06 01:33:47 +0200

    handleChange update

--- client/src/api/users.js
+++ client/src/api/users.js
@@ -1,16 +1,16 @@
 const API_URL = "http://localhost:4000";
 
 export const getUsers = () => {
-  return fetch(`${API_URL}/api/user`).then(response => response.json());
+  return fetch(`${API_URL}/user`).then(response => response.json());
 };
 
 export const searchUsers = query => {
-  return fetch(`${API_URL}/api/user/search?text=${query}`).then(response =>
+  return fetch(`${API_URL}/user/search?text=${query}`).then(response =>
     response.json()
   );
+};
 
-  
-  // for login page
+// for login page
 
 export const loginuser = (fname, lname, email, pword) => {
   return fetch(`${API_URL}/user/login`, {
