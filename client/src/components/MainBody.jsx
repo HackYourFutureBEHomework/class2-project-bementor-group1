1d606830e1b237fe49cd2357714d4154d8b0baca
Joel <camacho-joel@hotmail.com>
2019-04-10 22:58:27 +0200

    adding text and buttom

--- client/src/components/MainBody.jsx
+++ client/src/components/MainBody.jsx
@@ -2,7 +2,21 @@ import React, { Component } from "react";
 
 class MainBody extends Component {
   render() {
-    return <main className="main-backgroud" />;
+    return (
+      <main className="main-container">
+        <div className="main-background"> </div>
+
+        <h1 className="main-title">Welcome</h1>
+        <p className="main-text">
+          {" "}
+          BeMentor is an open source mentorship platform to connect women in
+          technology.
+        </p>
+        <a href="" className="main-buttom">
+          Register now!
+        </a>
+      </main>
+    );
   }
 }
 export default MainBody;
